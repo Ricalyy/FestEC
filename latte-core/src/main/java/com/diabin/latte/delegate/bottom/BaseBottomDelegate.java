f1c17613b290142151dbf5166832ede118cbd92a
huangchencong <huangchencong93@163.com>
2018-09-27 01:27:31 +0800

    底部导航栏1
    Signed-off-by: huangchencong <huangchencong93@163.com>

---
+++ latte-core/src/main/java/com/diabin/latte/delegate/bottom/BaseBottomDelegate.java
@@ -0,0 +1,52 @@
+package com.diabin.latte.delegate.bottom;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.annotation.Nullable;
+
+import com.diabin.latte.delegate.LatteDelegate;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by huangchencong on 2018/9/26.
+ */
+
+public abstract class BaseBottomDelegate extends LatteDelegate {
+
+    private final ArrayList<BottomItemDelegate> ITEM_DELEGATES = new ArrayList<>();
+    private final ArrayList<BottomTabBean> TAB_BEANS = new ArrayList<>();
+    private final LinkedHashMap<BottomTabBean, BottomItemDelegate> ITEMS = new LinkedHashMap<>();
+    private int mCurrentDelegate = 0;
+    private int mIndexDelegate = 0;
+    private int mClickedColor = Color.RED;
+
+    public abstract LinkedHashMap<BottomTabBean, BottomItemDelegate> setItems(ItemBuilder builder);
+
+    public abstract int setIndexDelegate();
+
+    @ColorInt
+    public abstract int setClickedColor();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mIndexDelegate = setIndexDelegate();
+        if (setClickedColor() != 0) {
+            mClickedColor = setClickedColor();
+        }
+
+        final ItemBuilder builder = ItemBuilder.builder();
+        final LinkedHashMap<BottomTabBean, BottomItemDelegate> items = setItems(builder);
+        ITEMS.putAll(items);
+        for (Map.Entry<BottomTabBean, BottomItemDelegate> item : ITEMS.entrySet()) {
+            final BottomTabBean key = item.getKey();
+            final BottomItemDelegate value = item.getValue();
+            TAB_BEANS.add(key);
+            ITEM_DELEGATES.add(value);
+        }
+    }
+}
---
+++ latte-core/src/main/java/com/diabin/latte/delegate/bottom/BottomTabBean.java
@@ -0,0 +1,24 @@
+package com.diabin.latte.delegate.bottom;
+
+/**
+ * Created by huangchencong on 2018/9/26.
+ */
+
+public final class BottomTabBean {
+    private final CharSequence ICON;
+    private final CharSequence TITLE;
+
+    public BottomTabBean(CharSequence icon, CharSequence title) {
+        this.ICON = icon;
+        this.TITLE = title;
+    }
+
+    public CharSequence getIcon() {
+        return ICON;
+    }
+
+    public CharSequence getTitle() {
+        return TITLE;
+    }
+
+}
